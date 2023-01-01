e74e1f1bfa4b3a9d47b461dfad1b605983e432f0
Shubham sahu <71839040+shubhamsahu03@users.noreply.github.com>
2023-01-01 10:55:17 +0530

    last commit

--- pandas_tut.py
+++ pandas_tut.py
@@ -1,63 +1,13 @@
-"""import csv
-flag=True
-while flag:
-    a=input("Enter deptid:")
-    b=input("Enter name")
-    c=input("List of batches:")
-    with open("csv_files\department.csv","a") as csv_file:
-        csv_writer=csv.writer(csv_file)
-        csv_writer.writerow([a,b,list(c)])
-    csv_file.close()    
-    flag=bool(int(input("Enter 1 to contnue\n2. 0 to Exit")))
-"""
-
-
-"""
-import ast
-
-string="['PG22','PG20']"
-list_=ast.literal_eval(string)
-print(list_)"""
-
-"""from tkinter import *
-
-def validate(string):
-    # Return False if the string is only digits, True otherwise
-    return not string.isdigit()
-
-root = Tk()
-
-entry = Entry(root, validate="key", validatecommand=(root.register(validate), "%S"))
-entry.pack()
-
-root.mainloop()"""
-
-
-'''import pandas as pd
-df=pd.read_csv("csv_files\students.csv")
-
-in_lst=df.loc[df[df.columns[3]]=="CSE(AIML)22"]
-
-
-
-print(in_lst)    '''
-
-
-'''excel_file=r"csv_files/exam.csv"
-df=pd.read_csv(excel_file)
-x=input("Enter course ID:")
-y=input("Enter Name")
-filtered_df = df.loc[(df['Course ID'] ==x) & (df["Student ID"]==y),["Student ID","Marks"]]
-print("Result is:",list(itertools.chain(*filtered_df.to_numpy().tolist())))'''
-
-
 
 
 from texttable import Texttable
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-import itertools,ast
+import itertools
+import ast
+
+
 def get_list_of_pct(dictionary):
     pct_lst = []
     for key in dictionary:
@@ -73,29 +23,6 @@ def get_list_of_pct(dictionary):
         return pct_lst
     else:
         return ["NA"]
-
-
-'''tableObj=Texttable()
-tableObj.set_cols_align(["l", "r", "c"])
-  
-# Set datatype of each column
-tableObj.set_cols_dtype(["a", "i", "t"])
-  
-# Adjust columns
-tableObj.set_cols_valign(["t", "m", "b"])
-  
-# Insert rows
-tableObj.add_rows([
-        ["ORGANIZATION", "ESTABLISHED", "CEO"],
-        ["Google", 1998, "Sundar Pichai"],
-        ["Microsoft", 1975, "Satya Nadella"],
-        ["Nokia", 1865, "Rajeev Suri"],
-        ["Geeks for Geeks", 2008, "Sandeep Jain"],
-        ["HackerRank", 2007, "Vivek Ravisankar"]
-        ])
-  
-# Display table
-print(tableObj.draw())'''
 
 
 def report_card(student_id):
@@ -145,7 +72,6 @@ def report_card(student_id):
 
 
 # Create the scatter plot
-
 
 
 # create a dictionary of courses and studentID-marks
@@ -208,26 +134,35 @@ def create_scatter_plot(csv_filename):
     plt.ylabel('Batches')
     plt.title('Scatterplot of marks and batches by courses')
     plt.show()
+
+
 def create_lineplot():
     pass
+
+
 def get_marks(course_id, student_id):
-        excel_file = r"csv_files/exam.csv"
-        df = pd.read_csv(excel_file)
+    excel_file = r"csv_files/exam.csv"
+    df = pd.read_csv(excel_file)
 
-        filtered_df = df.loc[(df['Course ID'] == course_id) & (
-            df["Student ID"] == student_id), ["Student ID", "Marks"]]
+    filtered_df = df.loc[(df['Course ID'] == course_id) & (
+        df["Student ID"] == student_id), ["Student ID", "Marks"]]
 
-        return list(itertools.chain(*filtered_df.to_numpy().tolist()))
+    return list(itertools.chain(*filtered_df.to_numpy().tolist()))
+
 
 def get_lst_of_courses(batchid):
-    df=pd.read_csv("csv_files/Batches.csv")
-    filtered_df=df.loc[df["Batch ID"]==batchid]
+    df = pd.read_csv("csv_files/Batches.csv")
+    filtered_df = df.loc[df["Batch ID"] == batchid]
     return ast.literal_eval(filtered_df.to_numpy().tolist()[0][3])
+
+
 def get_lst_of_batches(deptid):
-    df=pd.read_csv("csv_files/department.csv")
-    filtered_df=df.loc[df["Department ID"]==deptid,["Batches"]]
+    df = pd.read_csv("csv_files/department.csv")
+    filtered_df = df.loc[df["Department ID"] == deptid, ["Batches"]]
     return ast.literal_eval(filtered_df.to_numpy().tolist()[0][0])
-#print(get_lst_of_batches("CSE(AIML)"))    
+# print(get_lst_of_batches("CSE(AIML)"))
+
+
 def get_average(lst):
     total = 0
     for number in lst:
